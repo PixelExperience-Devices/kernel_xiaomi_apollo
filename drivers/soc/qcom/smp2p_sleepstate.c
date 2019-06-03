6f5495913eea6db3644fe47cfa06b9cacf69f46c
Chris Lew <clew@codeaurora.org>
2019-06-03 11:47:57 -0700

    soc: qcom: smp2p_sleepstate: Add suspend delay
    Add delay in smp2p_sleepstate suspend path to prevent wakeup loop with
    non wakeup streaming from sensors. This delay should be the maximum
    time for sensors samples in flight to be drained.
    
    Change-Id: I79f944caa2ccdd65dc1649aef8d6359f44612479
    Signed-off-by: Chris Lew <clew@codeaurora.org>
    Signed-off-by: Ananth Raghavan Subramanian <sananth@codeaurora.org>
    Signed-off-by: Kelly Rossmoyer <krossmo@google.com>
    Bug: 123377615
    Bug: 131260677
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/soc/qcom/smp2p_sleepstate.c
+++ drivers/soc/qcom/smp2p_sleepstate.c
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2014-2020, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 #include <linux/module.h>
 #include <linux/suspend.h>
@@ -11,6 +20,7 @@
 #include <linux/of_irq.h>
 #include <linux/of.h>
 #include <linux/pm_wakeup.h>
+#include <linux/delay.h>
 
 #define PROC_AWAKE_ID 12 /* 12th bit */
 #define AWAKE_BIT BIT(PROC_AWAKE_ID)
@@ -32,6 +42,7 @@ static int sleepstate_pm_notifier(struct notifier_block *nb,
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
 		qcom_smem_state_update_bits(state, AWAKE_BIT, 0);
+		usleep_range(10000, 10500); /* Tuned based on SMP2P latencies */
 		break;
 
 	case PM_POST_SUSPEND:
